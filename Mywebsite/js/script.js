432aeb0847ee41dac46f1f469ec0500977b21b43
Rade <ramyadayalan14@gmail.com>
2025-03-19 15:13:40 +0000

    Delete Mywebsite directory

--- Mywebsite/js/script.js
+++
@@ -1,5 +0,0 @@
-// Simple JavaScript example
-document.querySelector('form').addEventListener('submit', function(e) {
-    e.preventDefault(); // Prevent form submission
-    alert('Your message has been sent!');
-});
