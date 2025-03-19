561b52a7ca4e054539b683aa4b26d817842f4a88
ramyadeena <ramyadayalan14@gmail.com>
2025-03-19 11:20:16 +0000

    Organize and add CSS and JavaScript files

---
+++ Mywebsite/js/script.js
@@ -0,0 +1,5 @@
+// Simple JavaScript example
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent form submission
+    alert('Your message has been sent!');
+});
